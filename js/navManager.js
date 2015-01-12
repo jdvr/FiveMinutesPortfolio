7e632caa7423c3cc02f69cea55c17aac0c7d7197
Juan David Vega <jdvr.93@hotmail.com>
2015-01-12 11:24:28 +0000

    first prototype

--- js/navManager.js
+++ js/navManager.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
 			$('#fullpage').fullpage({
 				anchors: ['firstPage', 'secondPage', '3rdPage'],
 				menu: '#menu',
-				sectionsColor: ['#FFFFFF', '#FFFFFF', '#7E8F7C'],
+				sectionsColor: ['#59ADEE', '#FFFFFF', '#7E8F7C'],
 				navigation: true,
 				slidesNavigation: true,
 				navigationPosition: 'right',
 				navigationTooltips: ['Inicio', 'Qienes Somos', 'Eventos'],
 				resize: false,
-				autoScrolling: false,
 			});
 		});
